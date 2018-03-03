32fadbef67c452f3fe37e0282a1137c375c1c62b
TNA_COMP\Tinc4n11 <timothy.sandridge@snhu.edu>
2018-03-03 14:00:42 -0500

    Assembled the level one objects
    Created a level one scene. Place the artwork, and reassembled the objects to match the original design. Applied the proper scripts to the level objects.

---
+++ LillyGame/Assets/Scripts/endgame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endgame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //End Game if all tagged objects are gone
+        if (GameObject.FindGameObjectsWithTag("TargetObject").Length <= 0)
+        {
+            Application.Quit();
+        }
+	}
+}
---
+++ LillyGame/Assets/TimothySTestStuff/clickcontrol.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickcontrol : MonoBehaviour {
+
+    public static string nameofobj;
+    public GameObject objnametext;
+    public static int objsleft = 4;
+    public Transform successclick;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        nameofobj = gameObject.name;
+        /* for debugging porposes
+          Debug.Log(nameofobj); */
+        Destroy(gameObject);
+        Destroy(objnametext);
+        //Particle effect where text used to be
+        Instantiate(successclick, objnametext.transform.position, successclick.rotation);
+        objsleft--;
+    }
+}
