7aa08be40797e9e9413d8433da327f788bfe7d57
zlin13 <zlin13@stevens.edu>
2018-03-05 10:37:23 -0500

    Add files via upload

---
+++ LillyGame/Assets/clickcontrol.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickcontrol : MonoBehaviour {
+    public static string nameofobj;
+    public GameObject objnametext;
+    public Transform objnametextPos;
+    public Transform sucessclick;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnMouseDown()
+    {
+        nameofobj = gameObject.name;
+        //Debug.Log(nameofobj);
+        Destroy(gameObject);
+        Destroy(objnametext);
+        trackingclicks.totalclicks = 0;
+        Instantiate(sucessclick, objnametextPos.position, sucessclick.rotation);
+    }
+}
