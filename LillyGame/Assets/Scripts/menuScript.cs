94c66c89a68cc7c1a081b810d8b8a4a19eae45ed
xzou0624 <36636986+xzou0624@users.noreply.github.com>
2018-03-18 05:51:29 -0400

    Start level adds more function
    each button now has its own function and does not overwrap each other. Credit and Instruction pop up window set up correctly. Music will continue looping without interuption even change the level, it will still playing without stopping.

--- LillyGame/Assets/Scripts/menuScript.cs
+++ LillyGame/Assets/Scripts/menuScript.cs
@@ -2,75 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class menuScript : MonoBehaviour
 {
-
-    public Image instruction_text;
-    public Image credit_text;
-    public Button start;
-    public Button credit;
-    public Button instruction;
-    public Button home;
-  
-
-
-    // Use this for initialization
-    void Start()
+    public void Playgame()
     {
-        start = start.GetComponent<Button>();
-        credit = credit.GetComponent<Button>();
-        instruction = instruction.GetComponent<Button>();
-        home = home.GetComponent<Button>();
-
-
-        instruction_text.enabled = false;
-        credit_text.enabled = false;
-
-    }
-
-    public void ins_press()
-    {
-        instruction.enabled = true;
-        start.enabled = true;
-        credit.enabled = true;
-        credit_text.enabled = false;
-        instruction_text.enabled = true;
-        home.enabled = true;
-
-        
-
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
 
-    public void credit_press()
+    public void QuitGame()
     {
-        credit.enabled = true;
-        start.enabled = true;
-        instruction.enabled = true;
-        credit_text.enabled = true;
-        instruction_text.enabled = false;
-        home.enabled = true;
-
+        Debug.Log("Quit!");
+        Application.Quit();
     }
-
-    public void back_home()
-    {
-        credit.enabled = true;
-        start.enabled = true;
-        instruction.enabled = true;
-        credit_text.enabled = false;
-        instruction_text.enabled = false;
-        home.enabled = true;
-    }
-
-
-    public void startlevel()
-    {
-        Application.LoadLevel(1);
-
-    }
-
-
 }
---
+++ LillyGame/Assets/Scripts/pop_up_window.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pop_up_window : MonoBehaviour {
+
+    public GameObject window_instruction;
+    public GameObject window_credit;
+    public Button home;
+    void Start()
+    {
+        home = home.GetComponent<Button>();
+    }
+    public void Show_ins(string message){
+		//messageField.text = message;
+		window_instruction.SetActive (true);
+        window_credit.SetActive(false);
+	}
+
+    public void Show_cre(string message)
+    {
+        //messageField.text = message;
+        window_instruction.SetActive(false);
+        window_credit.SetActive(true);
+    }
+    public void Hide(){
+        window_instruction.SetActive(false);
+        window_credit.SetActive(false);
+    }
+
+    public void backhome()
+    {
+        home.enabled = true;
+        window_instruction.SetActive(false);
+        window_credit.SetActive(false);
+    }
+
+}
