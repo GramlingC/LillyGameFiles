fac22d5bfe8da69520737428681780c4cc9986b6
zlin13 <zlin13@stevens.edu>
2018-04-01 14:32:09 -0400

    Add files via upload

--- LillyGame/Assets/Scripts/movetowards.cs
+++ LillyGame/Assets/Scripts/movetowards.cs
@@ -2,29 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movetowards : MonoBehaviour {
+public class movetowards : MonoBehaviour
+{
 
     public Transform target;
     public float speed;
     Vector3 originalPos;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         //Remember where it started
         originalPos = gameObject.transform.position;
-	}
+    }
 
     // Update is called once per frame
     void Update()
     {
         //move towards a specfied position
         float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+       transform.position = Vector3.MoveTowards(transform.position, target.position, step);
 
         //check if object has reached its destination
         if (gameObject.transform.position == target.position)
         {
-            ResetPosition();
+          return;
         }
     }
 
@@ -33,5 +35,9 @@ public class movetowards : MonoBehaviour {
     {
         gameObject.transform.position = originalPos;
 
+    }
+    void OnMouseDown()
+    {
+        ResetPosition();
     }
 }
