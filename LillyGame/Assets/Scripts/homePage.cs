96df695fe2d326a926463c29a5e34aff12a6057a
xzou0624 <36636986+xzou0624@users.noreply.github.com>
2018-04-15 05:09:27 -0400

    4/15 Mark's update
    I added numbers in level-select level and changed the font in the game. Layouts have been changed and some assets replaced. Organized level1, level2, select level, and begin level.

---
+++ LillyGame/Assets/Scripts/homePage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class homePage : MonoBehaviour {
+
+	// Use this for initialization
+    public void home_page()
+    {
+        SceneManager.LoadScene("StartLevel");
+    }
+}
