76ec48cbdce992982e0f4afb4182822a2a979361
TNA_COMP\Tinc4n11 <timothy.sandridge@snhu.edu>
2018-08-30 14:04:49 -0400

    "RateMe" popup
    Doing the rating prompt was as simple as Mark's link to the barrie patch website. Application.OpenURL("whatever site")
    
    After the player pushes the yes button, PlayerPrefs("haveRated") is set to 1 and the prompt only opens if "haveRated"==0.
    
    Prompt also only opens after level two is completed, this is a bit of time for the player to get acquainted. It can be changed.
    
    Prompt always opens if level two is completed and player has not yet rated, idk if this is too annoying or in their face. Maybe if it opened on a later level, or if I put in a delay until it opens again?

---
+++ LillyGame/Assets/Scripts/RateMe.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RateMe : MonoBehaviour {
+
+    public GameObject rateObject;
+
+	// Use this for initialization
+	void Start () {
+        rateObject.SetActive(false);
+        //PlayerPrefs.SetInt("haveRated", 0);//debugging
+
+        if (PlayerPrefs.GetInt("rewardAnimal_2") == 1)
+        {
+            if (PlayerPrefs.GetInt("haveRated") == 0)
+            {
+                rateObject.SetActive(true);
+            }
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Rate()
+    {
+        PlayerPrefs.SetInt("haveRated", 1);
+        Application.OpenURL("https://thebarriepatch.com/about-us");
+        rateObject.SetActive(false);
+        /* Replace open url with: 
+         * Application.OpenURL("market://details?id=YOUR_ID");
+         * replace YOUR-ID with our id
+         */
+        
+
+    }
+
+    public void Close()
+    {
+        rateObject.SetActive(false);
+    }
+}
