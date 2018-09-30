1db98a80087496b3e4f241d242f291aa8d62848e
TNA_COMP\Tinc4n11 <timothy.sandridge@snhu.edu>
2018-09-30 14:30:21 -0400

    popup to prompt player about cat selection

---
+++ LillyGame/Assets/Scripts/UnlockInfoPanel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockInfoPanel : MonoBehaviour {
+
+    public GameObject Panel; // The panel we want to display and hide
+
+	// Use this for initialization
+	void Start () {
+         Panel.SetActive(false); //Always have it off
+         //PlayerPrefs.SetInt("hasSeenInfoPanel", 0); //debugging
+
+
+        if (PlayerPrefs.GetInt("rewardAnimal_1") == 1) //If level one is complete
+        {
+            if (PlayerPrefs.GetInt("hasSeenInfoPanel") == 0) //Only need to view it once
+            {
+                Panel.SetActive(true); //Display panel
+            }
+        }
+
+    }
+
+    public void Close()
+    {
+        PlayerPrefs.SetInt("hasSeenInfoPanel", 1); //Record that is has been displayed
+        Panel.SetActive(false); //Turn it off
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
